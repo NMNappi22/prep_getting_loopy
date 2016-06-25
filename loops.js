792097424206bed5c28304759f9ed3aaf8767d27
NMNappi22 <NMNappi22@gmail.com>
2016-06-24 15:00:02 -1000

    W2D5

--- loops.js
+++ loops.js
@@ -1,7 +1,21 @@
 // Do your work here
-var Presidents = [Washington, Adams, Jefferson, Madison, Monroe];
 
-for(var i = 0; i < Presidents.length; i++){
-  console.log("The value of i is: " + [i]);
-  console.log("The value at index is: " + Presidents[i]);
+/**
+function listPresidents(presidents){
+	for(var i = 0; i < presidents.length; i++){
+	  console.log("The value of i is: " + [i]);
+	  console.log("The value at index is: " + presidents[i]);
+	}
 }
+var Presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+listPresidents(Presidents);
+*/
+
+
+function stringOfNumber(stringOfNumbers){
+	for(var i = 10; stringOfNumbers.length === 20; i++){
+		console.log(i)
+	}
+}
+var SOF = ['']
+stringOfNumber(SOF)
